448e7381f83fadec7d9e4b5448f8e03ae0afdb9f
Yermak Diana <d.yermak.ia23@kpi.ua>
2025-04-21 17:25:20 +0300

    lab6

--- main.tf
+++ main.tf
@@ -3,31 +3,18 @@ provider "aws" {
 }
 
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-security-group"
-  description = "Allow HTTP and SSH traffic"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+data "aws_security_group" "web_sg_existing" {
+  filter {
+    name   = "group-name"
+    values = ["web-security-group"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-032a110e561ba32e8"]
   }
 }
+
 
 
 resource "aws_instance" "web_instance" {
