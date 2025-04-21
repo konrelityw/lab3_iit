0a68af65906e468e68cbc5b0249d543e0c51a982
Yermak Diana <d.yermak.ia23@kpi.ua>
2025-04-21 17:21:33 +0300

    lab6

--- main.tf
+++ main.tf
@@ -3,7 +3,31 @@ provider "aws" {
 }
 
 
-terraform import aws_security_group.web_sg sg-0124f1d16b96ea8a0
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 resource "aws_instance" "web_instance" {
